797dc3d52605aee623b887fb132496ad11d3486b
mrkulk <tejasdkulkarni@gmail.com>
2015-08-26 13:29:34 -0400

    first commit

---
+++ src/main.lua
@@ -0,0 +1,96 @@
+-- Tejas D Kulkarni (tejask@mit.edu)
+
+require('cunn')
+require('nngraph')
+require('base')
+
+-- Trains 1h and gives test 115 perplexity.
+params = {batch_size=20,
+                seq_length=20,
+                layers=2,
+                decay=2,
+                rnn_size=200,
+                dropout=0,
+                init_weight=0.1,
+                lr=1,
+                input_dim=100,
+                max_epoch=4,
+                max_max_epoch=13,
+                max_grad_norm=5}
+
+require('model')
+require('runner')
+require('utils')
+
+
+local state_train, state_valid, state_test
+local model = {}
+local paramx, paramdx
+
+local function main()
+  g_init_gpu(arg)
+  state_train = {data=transfer_data(ptb.traindataset(params.batch_size))}
+  state_valid =  {data=transfer_data(ptb.validdataset(params.batch_size))}
+  state_test =  {data=transfer_data(ptb.testdataset(params.batch_size))}
+  print("Network parameters:")
+  print(params)
+  local states = {state_train, state_valid, state_test}
+  for _, state in pairs(states) do
+    reset_state(state)
+  end
+  setup()
+  local step = 0
+  local epoch = 0
+  local total_cases = 0
+  local beginning_time = torch.tic()
+  local start_time = torch.tic()
+  print("Starting training.")
+  local words_per_step = params.seq_length * params.batch_size
+  local epoch_size = torch.floor(state_train.data:size(1) / params.seq_length)
+  local perps
+  while epoch < params.max_max_epoch do
+    local perp = fp(state_train)
+    if perps == nil then
+      perps = torch.zeros(epoch_size):add(perp)
+    end
+    perps[step % epoch_size + 1] = perp
+    step = step + 1
+    bp(state_train)
+    total_cases = total_cases + params.seq_length * params.batch_size
+    epoch = step / epoch_size
+    if step % torch.round(epoch_size / 10) == 10 then
+      local wps = torch.floor(total_cases / torch.toc(start_time))
+      local since_beginning = g_d(torch.toc(beginning_time) / 60)
+      print('epoch = ' .. g_f3(epoch) ..
+            ', train perp. = ' .. g_f3(torch.exp(perps:mean())) ..
+            ', wps = ' .. wps ..
+            ', dw:norm() = ' .. g_f3(model.norm_dw) ..
+            ', lr = ' ..  g_f3(params.lr) ..
+            ', since beginning = ' .. since_beginning .. ' mins.')
+    end
+    if step % epoch_size == 0 then
+      run_valid()
+      if epoch > params.max_epoch then
+          params.lr = params.lr / params.decay
+      end
+    end
+    if step % 33 == 0 then
+      cutorch.synchronize()
+      collectgarbage()
+    end
+  end
+  run_test()
+  print("Training is over.")
+end
+
+-- main()
+
+core_network = create_network()
+print(params)
+print(
+  core_network:forward({
+    torch.zeros(params.batch_size, params.rnn_size):cuda(),
+    torch.zeros(params.batch_size):cuda(),
+    {torch.zeros(params.batch_size, params.rnn_size):cuda(), torch.zeros(params.batch_size, params.rnn_size):cuda(), torch.zeros(params.batch_size, params.rnn_size):cuda(), torch.zeros(params.batch_size, params.rnn_size):cuda() }
+  })
+)
---
+++ src/model.lua
@@ -0,0 +1,56 @@
+
+function lstm(x, prev_c, prev_h)
+  -- Calculate all four gates in one go
+  local i2h = nn.Linear(params.rnn_size, 4*params.rnn_size)(x)
+  local h2h = nn.Linear(params.rnn_size, 4*params.rnn_size)(prev_h)
+  local gates = nn.CAddTable()({i2h, h2h})
+  
+  -- Reshape to (batch_size, n_gates, hid_size)
+  -- Then slize the n_gates dimension, i.e dimension 2
+  local reshaped_gates =  nn.Reshape(4,params.rnn_size)(gates)
+  local sliced_gates = nn.SplitTable(2)(reshaped_gates)
+  
+  -- Use select gate to fetch each gate and apply nonlinearity
+  local in_gate          = nn.Sigmoid()(nn.SelectTable(1)(sliced_gates))
+  local in_transform     = nn.Tanh()(nn.SelectTable(2)(sliced_gates))
+  local forget_gate      = nn.Sigmoid()(nn.SelectTable(3)(sliced_gates))
+  local out_gate         = nn.Sigmoid()(nn.SelectTable(4)(sliced_gates))
+
+  local next_c           = nn.CAddTable()({
+      nn.CMulTable()({forget_gate, prev_c}),
+      nn.CMulTable()({in_gate,     in_transform})
+  })
+  local next_h           = nn.CMulTable()({out_gate, nn.Tanh()(next_c)})
+
+  return next_c, next_h
+end
+
+function create_network()
+  local x                = nn.Identity()()
+  local y                = nn.Identity()()
+  local prev_s           = nn.Identity()()
+
+  local i                = {[0] = nn.Identity()(x)}
+
+  local next_s           = {}
+  local split         = {prev_s:split(2 * params.layers)}
+  for layer_idx = 1, params.layers do
+    local prev_c         = split[2 * layer_idx - 1]
+    local prev_h         = split[2 * layer_idx]
+    local dropped        = nn.Dropout(params.dropout)(i[layer_idx - 1])
+    local next_c, next_h = lstm(dropped, prev_c, prev_h)
+    table.insert(next_s, next_c)
+    table.insert(next_s, next_h)
+    i[layer_idx] = next_h
+  end
+
+  local h2y              = nn.Linear(params.rnn_size, params.input_dim)
+  local dropped          = nn.Dropout(params.dropout)(i[params.layers])
+  local pred             = nn.LogSoftMax()(h2y(dropped))
+  local err              = nn.ClassNLLCriterion()({pred, y})
+  local module           = nn.gModule({x, y, prev_s},
+                                      {err, nn.Identity()(next_s)})
+  
+  module:getParameters():uniform(-params.init_weight, params.init_weight)
+  return transfer_data(module)
+end
